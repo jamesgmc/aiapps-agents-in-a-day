60b1b7962d1b0cc38b272633d39ed9792ad53237
qkfang@hotmail.com <qkfang@hotmail.com>
2025-08-06 10:26:26 +1000

    setup docs

---
+++ docs/90-Feedback.md
@@ -0,0 +1,7 @@
+# Your Feedback
+
+We are looking forward to hear your feedback about our `Agentic AI APP in one-day` ! 
+
+Your feedback will help us make necessary improvements. 
+
+Thank you so much for your support!
---
+++ docusaurus.config.js
@@ -0,0 +1,98 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Agentic AI APP In a Day Hackathon",
+  tagline:
+    "Accelerate Your AI Journey: Build an Agentic AI APP",
+  url: "https://github-insight-anz-lab.github.io",
+  baseUrl: "/aiapp1day/",
+
+  onBrokenLinks: "ignore",
+  onBrokenMarkdownLinks: "ignore",
+  trailingSlash: true,
+
+  organizationName: "GitHub-Insight-ANZ-Lab",
+  projectName: "aiapp1day",
+  deploymentBranch: "main",
+  favicon: "img/favicon.ico",
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      docs: {
+        sidebar: {
+          hideable: false,
+          autoCollapseCategories: false,
+        },
+      },
+
+      navbar: {
+        title: "Build an Agentic AI APP",
+        logo: {
+          alt: "Workshop: Build an Agentic AI APP",
+          src: "img/logo-ws3.png",
+        },
+        items: [
+          {
+            type: "localeDropdown",
+            position: "right",
+          },
+        ],
+      },
+
+      footer: {
+        style: "dark",
+        links: [],
+        copyright: `Copyright © ${new Date().getFullYear()} Learn how to interact with OpenAI models.`,
+      },
+
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+
+  plugins: [
+    [
+      "@docusaurus/plugin-ideal-image",
+      {
+        quality: 96,
+        max: 1000, // max resized image's size.
+        min: 420, // min resized image's size.
+        steps: 4, // #images b/w min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
+  ],
+};
+
+module.exports = config;
