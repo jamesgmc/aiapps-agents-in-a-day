d05a87f31e7213942be2da059dfb236d5a5b9e79
Daniel Fang <qkfang@hotmail.com>
2025-09-13 23:31:33 +1000

    add

---
+++ labs/20-Chatbot/start/3a-vector-search.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+// set up the MongoDB client
+const dbClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
+var dbname = process.env.MONGODB_Name;
+
+async function main() {
+    try {
+        await dbClient.connect();
+        console.log("Connected to MongoDB");
+
+        // TODO: Set up Azure Cosmos DB vector store and perform vector search
+        // This will be added during the lab exercise
+
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await dbClient.close();
+        console.log('Disconnected from MongoDB');
+    }
+}
+
+main().catch(console.error);
