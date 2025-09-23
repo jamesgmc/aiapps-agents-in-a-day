3dc4ef727ed73617b9db83a9506ad103346f86e5
Daniel Fang <138417504+qkfang@users.noreply.github.com>
2025-09-23 09:33:21 +0000

    add

--- labs/20-Chatbot/3a-vector-search.js
+++ labs/20-Chatbot/3a-vector-search.js
@@ -10,15 +10,15 @@ const { OpenAIEmbeddings } = require("@langchain/openai");
 const dbClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
 var dbname = process.env.MONGODB_Name;
 
+
+// set up the Azure Cosmos DB vector store using the initialized MongoDB client
+
+
 async function main() {
     try {
         await dbClient.connect();
         console.log("Connected to MongoDB");
 
-        // set up the Azure Cosmos DB vector store using the initialized MongoDB client
-
-
-        // perform a vector search using the vector store
 
 
     } catch (err) {
